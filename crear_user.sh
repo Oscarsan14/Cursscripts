a4b1ff09a5c4f4b775964c60c2fcf204361b6962
Oscar <oscarsan14@gmail.com>
2023-04-28 08:41:31 +0200

    28 de abirl

--- crear_user.sh
+++ crear_user.sh
@@ -1,16 +1,40 @@
 #!/bin/bash
 
-#Script per crear un usuari
-
-#primer comprovam si som l'usuari root
+function usage(){
+    echo "Usage ${0}"
+    echo
+    echo "has de ser root"
+    echo "l'usari no és pot reptir"
+    exit 1 
+}
 if [[ ${UID} -ne 0 ]]
 then
-   echo "Usage ${0}"
-   echo
-   echo "Has de ser root"
-   exit 1
-fi 
+    usage
+fi
+
+read -p "Introdueix el nom complet: " COMMENTS
 
 read -p "Introdueix el nom d'usuari: " USER_NAME
 
-echo "creant l'usuari ${USER_NAME}"
+read -p "Introdueix el password: " PASSWORD
+
+
+useradd -m -c "${COMMENTS}" ${USER_NAME} &> /dev/null
+
+if [[ ${?} -ne 0 ]]
+then
+    echo "Errada creat l'usuari"
+    exit 1
+fi
+
+echo "${USER_NAME}:${PASSWORD}" | chpasswd
+
+if [[ ${?} -ne 0 ]]
+then
+    echo "Errada Canviant password"
+    exit 1
+fi
+
+passwd -e ${USER_NAME}
+
+exit 0
---
+++ root1.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [[ ${UID} -ne 0 ]]
+then
+    echo "Usage ${0}"
+    echo
+    echo "Has de ser root"
+    exit 1
+fi
+
+read -p "Introdueix el nom d'usuari: " USER_NAME
+echo "Creant l'usuari ${USER_NAME}"
