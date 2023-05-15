669c950dbd8bccd925c27b3e3911821114a2a98d
Oscar <oscarsan14@gmail.com>
2023-05-15 10:43:25 +0200

    bots

--- Bot/telegram_bot_start.sh
+++ Bot/telegram_bot_start.sh
@@ -5,4 +5,4 @@ IP=$(curl ifconfig.me)
 echo ${IP}
 MISSATGE="La maquina ${HOST} se ha iniciado el dia $(date) con la ip: ${IP}"
 
-/home/oscar/cursscripts/Bot/telegram_bot_start.sh "${MISSATGE}"
+/home/oscar/cursscripts/Bot/telegram_bot_2.sh "${MISSATGE}"
